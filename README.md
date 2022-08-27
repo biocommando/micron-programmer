cfb2c6fa0b331def10daff6e0a49389307a75024
joonas <joonas1000@gmail.com>
2022-08-27 14:05:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Alesis Micron / MiniAk editor
+
+It's not the most beautiful thing in the world but it works right from your browser and lets you control all the midi-controllable parameters in Alesis Micron.
+
+The parameter JSON is based on the 'Common FAQ' article on the Ion-Micron-Miniak wiki at Fandom (https://ion-micron-miniak.fandom.com/wiki/Common_FAQ#MIDI_implementation) and is licensed under the Creative Commons Attribution-Share Alike License.
+
+Otherwise the tool is MIT licensed.
---
+++ midi.js
@@ -0,0 +1,75 @@
+
+const midi = {
+    getDevices: function (callback) {
+        if (this.devices !== undefined) {
+            callback(this.devices);
+            return;
+        }
+        navigator.requestMIDIAccess({ sysex: true }).then(midiAccess => {
+            const devices = [];
+            midiAccess.outputs.forEach(o => devices.push(o));
+            this.devices = devices;
+            callback(devices);
+        });
+    },
+    setDevice: function (name, onDone) {
+        this.getDevices(devices => {
+            this.device = devices.find(d => d.name === name);
+            onDone(this.device !== undefined);
+        });
+    },
+    noteOn: function (note, channel, velocity) {
+        var midiCmd = 0b10010000;
+        if (note < 128 && note >= 0) {
+            this.midiCmd([midiCmd | channel, note, velocity !== undefined ? velocity : 127]);
+        }
+    },
+    noteOff: function (note, channel) {
+        var midiCmd = 0b10000000;
+        if (note < 128 && note >= 0) {
+            this.midiCmd([midiCmd | channel, note, 0]);
+        }
+    },
+    midiCmd: function (cmd) {
+        setTimeout(() => this.device.send(cmd), 0);
+    }
+}
+
+function createNrpnParam (num) {
+    let ch = 0
+    const range = {min:0, max:100}
+    let cachedValue = undefined
+    return {
+        setChannel: c => ch = c,
+        send: value => {
+            value = range.min + (range.max - range.min) * value
+            value = Math.floor(value)
+            value = Math.min(value, (1 << 13) - 1)
+            value = Math.max(value, ~((1 << 13) - 1))
+
+            if (cachedValue === value)
+                return
+            cachedValue = value
+
+            const cc = (ccnum, ccval) => [0xB0 | ch, ccnum, ccval]
+            midi.midiCmd(cc(98, num & 0x7F))
+            midi.midiCmd(cc(99, (num >> 7) & 0x7F))
+            midi.midiCmd(cc(38, value & 0x7F))
+            midi.midiCmd(cc(6, (value >> 7) & 0x7F))
+        },
+        name: 'unknown',
+        getRange: () => range
+    }
+}
+
+function mapNrpnParamArray(arr) {
+    return arr.map(param => {
+        const nrpn = createNrpnParam(Number(param.num))
+        nrpn.name = param.name
+        nrpn.options = param.options
+        const range = nrpn.getRange()
+        range.min = Number(param.tmin)
+        range.max = Number(param.tmax)
+        return nrpn
+    })
+}
